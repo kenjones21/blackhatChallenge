c98b3a54fdbb8bbd9f3f9f0842b67a273ebb01d5
kenjones21 <kenjones22193@aim.com>
2015-05-14 16:51:06 -0400

    Added some helper functions

--- tools/decrypt.py
+++ tools/decrypt.py
@@ -25,8 +25,33 @@ def decVigenere(cText, key):
     A = ord('A')
     for i in range(0, len(key)):
         shifts.append(ord(key[i]) - A)
+        shifts[i] = (26 - shifts[i]) % 26 # Key used to encrypt, not decrypt
     for c in range(0, len(cText)):
         ans += chr((ord(cText[c]) - A + shifts[c % len(key)]) % 26 + A)
     return ans
 
+def decHill(cText, key):
+    ans = ""
+    decKey = matInverse(key);
+    numText = map(ord, cText)
+    # Do matrix multiplication here
+    return 0
 
+def matInverse(matrix):
+    det = inverse((matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]))
+    ans = [[0,0],[0,0]]
+    ans[0][0] = (det * matrix[1][1]) % 26
+    ans[1][1] = (det * matrix[0][0]) % 26
+    ans[1][0] = (det * -matrix[0][1]) % 26
+    ans[0][1] = (det * -matrix[1][0]) % 26
+    return ans
+
+def inverse(num):
+    """Calculates the (multiplicative) inverse of a number mod 26"""
+    for i in range(0,26): # I know, I know, could use EA or something, lazy. 
+        if (i * num) % 26 == 1:
+            return i
+    print("No inverse for this number!")
+    return 0 # Should make things obvious for our purposes. 
+
+print(matInverse([[3,2],[3,5]]))
